35a4169f1cc4d1cc6ec1f0fb7859226c1b642b63
levisn1 <djlevisn1@gmail.com>
2018-08-07 16:43:12 +0200

    fixing aws region issue

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -3,9 +3,10 @@ if Rails.env.production?
     config.fog_credentials = {
       # Configuration for Amazon S3
       :provider              => 'AWS',
-      :aws_access_key_id     => ENV['AKIAJK7VBZOCGC3ZPTCA'],
-      :aws_secret_access_key => ENV['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']
+      :aws_access_key_id     => ENV['S3_ACCESS_KEY'],
+      :aws_secret_access_key => ENV['S3_SECRET_KEY'],
+      :region                => ENV['S3_REGION']
     }
-    config.fog_directory     =  ENV['sampleappimageuploaderluiscarlosquarta']
+    config.fog_directory     =  ENV['S3_BUCKET']
   end
 end
